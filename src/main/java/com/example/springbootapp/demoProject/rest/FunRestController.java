4d6069ad261ffd85bff10543ece2afd3da2f84cb
satendrasinghrw <60536943+satendrasinghrw@users.noreply.github.com>
2024-02-08 18:51:28 +0530

    Injecting custom application properties

--- src/main/java/com/example/springbootapp/demoProject/rest/FunRestController.java
+++ src/main/java/com/example/springbootapp/demoProject/rest/FunRestController.java
@@ -1,11 +1,24 @@
 package com.example.springbootapp.demoProject.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    // Injecting properties from application.properties by the fields coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Exposing new endpoint for teamInfo
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName +", Team: "+ teamName;
+    }
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
